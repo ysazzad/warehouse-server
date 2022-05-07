21321bc1128a29a32e14287b47abe35e8ca6d01c
sazzad <hossensazzad1201@gmail.com>
2022-05-08 01:47:00 +0600

    single data fetch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -23,6 +23,13 @@ async function run() {
             const cursor = itemsCollection.find(query)
             const items = await cursor.toArray()
             res.send(items)
+        })
+
+        app.get("/items/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const items = await itemsCollection.findOne(query);
+            res.send(items);
         })
 
     }
