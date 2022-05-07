3b7ce4c4cae98f0e185129543ddaad1e0ad4d97d
sazzad <hossensazzad1201@gmail.com>
2022-05-07 22:20:56 +0600

    connect db and data display on ui

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dajix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const itemsCollection = client.db('warehouseBike').collection('items')
+
+        app.get("/items", async (req, res) => {
+            const query = {}
+            const cursor = itemsCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send("server is on running")
