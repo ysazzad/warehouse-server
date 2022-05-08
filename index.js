feab007350831619d78415288168209715b8dfb0
sazzad <hossensazzad1201@gmail.com>
2022-05-08 21:27:09 +0600

    add item create

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ async function run() {
             const items = await itemsCollection.findOne(query);
             res.send(items);
         })
+        //post
+        app.post("/items", async (req, res) => {
+            const newItems = req.body;
+            const result = await itemsCollection.insertOne(newItems);
+            res.send(result)
+        })
+        //delete
+        app.delete("/items/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query)
+            res.send(result)
+
+        })
 
     }
     finally {
